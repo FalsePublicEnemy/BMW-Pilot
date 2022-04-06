c30286683057f1e12c2f0280135391fdec45bb20
Karim Nassar <72457733+FalsePublicEnemy@users.noreply.github.com>
2022-04-06 22:36:20 +0200

    Refactor code. Make code more RESTful

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from api.v1 import views
 
 urlpatterns = [
-    path('qr/', views.get_banana)
+    path('qr/', views.CarView.as_view()),
+    path('test/', views.index, name='index'),
 ]
--- bmw/settings.py
+++ bmw/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,7 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/Users/karimnassar/Desktop/BMW/static/'
+
+STATIC_ROOT = '/Users/karimnassar/Desktop/BMW/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
