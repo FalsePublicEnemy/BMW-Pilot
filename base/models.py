53c1ed90c58f13ca525f676644c03ab65280822e
Karim Nassar <72457733+FalsePublicEnemy@users.noreply.github.com>
2022-04-07 16:22:22 +0200

    Fix patch method. Implement changing data in record

--- base/models.py
+++ base/models.py
@@ -1,6 +1,10 @@
+from typing import Tuple
 import uuid
 
 from django.db import models
+from rest_framework.request import Request
+
+from base.enums import VehicleParams
 
 
 class Vehicle(models.Model):
@@ -10,3 +14,9 @@ class Vehicle(models.Model):
 
     # TODO: prove specs for car. What's needed to be shown in description
 
+    @staticmethod
+    def update_fields(obj: 'Vehicle', request: Request) -> 'Vehicle':
+        request = request.data
+        obj.type = request['type']
+        # Add more vehicle params when data model is approved
+        return obj
